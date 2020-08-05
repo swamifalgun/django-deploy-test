ab93492daa73639a978bbb635fc6a1d27eeb0b30
falgun swami <swamifalgun@yahoo.com>
2020-08-05 23:33:32 +0300

    Modify database stuff

--- django_deploy_test/settings.py
+++ django_deploy_test/settings.py
@@ -87,12 +87,12 @@ WSGI_APPLICATION = 'django_deploy_test.wsgi.application'
 #     }
 # }
 
-DEBUG = config('DEBUG', default=False, cast=bool)
-DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
-}
+# DEBUG = config('DEBUG', default=False, cast=bool)
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default=config('DATABASE_URL')
+#     )
+# }
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
