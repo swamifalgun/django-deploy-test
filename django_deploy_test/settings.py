ccf769833de9059efb6f93b2e9ae8fb329eea33a
falgun swami <swamifalgun@yahoo.com>
2020-08-16 23:31:23 +0300

    Change db settings

--- django_deploy_test/settings.py
+++ django_deploy_test/settings.py
@@ -111,12 +111,12 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 
-# DEBUG = config('DEBUG', default=False, cast=bool)
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         default=config('DATABASE_URL')
-#     )
-# }
+DEBUG = config('DEBUG', default=False, cast=bool)
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
+}
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
